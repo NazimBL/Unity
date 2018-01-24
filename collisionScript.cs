3d0e02a0e4e7675129fa39ebcde9d291cafd64e9
NazimBL <30374106+NazimBL@users.noreply.github.com>
2018-01-24 19:37:42 +0100

    Characrter Controller Collision detect

---
+++ collisionScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionScript : MonoBehaviour {
+
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		
+		Debug.Log(hit.gameObject.name);
+		if (hit.gameObject.tag == "Foe") {
+			//Destroy(col.gameObject);
+			Debug.Log ("Player hit");
+			
+		}
+		
+	}
+}
